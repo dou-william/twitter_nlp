7c521284de5314182181498ff3d67e5e1a567ed8
RyanBetz <ryanbetz8484@gmail.com>
2022-10-14 17:19:26 -0400

    word cloud added

--- Word_Cloud.py
+++ Word_Cloud.py
@@ -16,3 +16,37 @@ for index, data in df.iterrows():
         print("Exception occurred at", index)
     counter += 1
 # Create and generate a word cloud image:
+comment_words = ''
+stopwords = set(STOPWORDS)
+
+# iterate through the csv file
+for val in df.OriginalTweet:
+
+    # typecast each val to string
+    val = str(val)
+
+    # split the value
+    tokens = val.split()
+    for sub_tokens in tokens:
+        if "https://t.co/" in sub_tokens:
+            tokens.remove(sub_tokens)
+        if "stock" in sub_tokens:
+            tokens.remove(sub_tokens)
+    # Converts each token into lowercase
+    for i in range(len(tokens)):
+        tokens[i] = tokens[i].lower()
+
+    comment_words += " ".join(tokens) + " "
+
+wordcloud = WordCloud(width=800, height=800,
+                      background_color='white',
+                      stopwords=stopwords,
+                      min_font_size=10).generate(comment_words)
+
+# plot the WordCloud image
+plt.figure(figsize=(8, 8), facecolor=None)
+plt.imshow(wordcloud)
+plt.axis("off")
+plt.tight_layout(pad=0)
+
+plt.show()
